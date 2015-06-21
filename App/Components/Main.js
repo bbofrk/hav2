82096d5660245d40cec22f0b7347273da37d4617
bbofrk <mingliwanglau@gmail.com>
2015-06-22 07:46:36 +1200

    tabview finished

--- App/Components/Main.js
+++ App/Components/Main.js
@@ -4,20 +4,60 @@ var React = require('react-native');
 var Parse = require('parse').Parse;
 var ParseReact = require('parse-react');
 var LinearGradient = require('react-native-linear-gradient');
+var FacebookTabBar = require('./FacebookTabBar');
+var deviceWidth = require('Dimensions').get('window').width;
+var ScrollableTabView = require('react-native-scrollable-tab-view');
+
+var FacebookLoginManager = require('NativeModules').FacebookLoginManager;
+var FBLogin = require('react-native-facebook-login');
+
+var Login = require('./Login');
+
+
 
 var {
   View,
   Text,
-  StyleSheet
+  StyleSheet,
+  ScrollView,
 } = React;
 
 var Main = React.createClass({
   render: function() {
     let blue = this.props.backgroundColor1;
     let white = this.props.backgroundColor2;
+    // console.log(Parse.User.current());
+    var _this = this;
     return (
       <LinearGradient colors={[blue, white]} style={styles.main}>
-        
+        <View style={styles.container}>
+          <ScrollableTabView renderTabBar={() => <FacebookTabBar />}>
+            <ScrollView tabLabel="ion|ios-paper" style={styles.tabView}>
+              <View style={styles.card}>
+                <Text>News</Text>
+              </View>
+            </ScrollView>
+            <ScrollView tabLabel="ion|person-stalker" style={styles.tabView}>
+              <View style={styles.card}>
+                <Text>Friends</Text>
+              </View>
+            </ScrollView>
+            <ScrollView tabLabel="ion|ios-chatboxes" style={styles.tabView}>
+              <View style={styles.card}>
+                <Text>Messenger</Text>
+              </View>
+            </ScrollView>
+            <ScrollView tabLabel="ion|ios-world" style={styles.tabView}>
+              <View style={styles.card}>
+                <Text>Notifications</Text>
+              </View>
+            </ScrollView>
+            <ScrollView tabLabel="ion|navicon-round" style={styles.tabView}>
+              <View style={styles.card}>
+              </View>
+            </ScrollView>
+          </ScrollableTabView>
+        </View>
       </LinearGradient>
     );
   }
@@ -26,9 +66,27 @@ var Main = React.createClass({
 var styles = StyleSheet.create({
   main: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  }
+  },
+  container: {
+    marginTop: 30,
+  },
+  tabView: {
+    width: deviceWidth,
+    padding: 10,
+    backgroundColor: 'rgba(0,0,0,0.01)',
+  },
+  card: {
+    borderWidth: 1,
+    backgroundColor: '#fff',
+    borderColor: 'rgba(0,0,0,0.1)',
+    margin: 5,
+    height: 150,
+    padding: 15,
+    shadowColor: '#ccc',
+    shadowOffset: {width: 2, height: 2},
+    shadowOpacity: 0.5,
+    shadowRadius: 3,
+  },
 });
 
 module.exports = Main;
--- index.ios.js
+++ index.ios.js
@@ -5,116 +5,17 @@
 'use strict';
 
 var React = require('react-native');
-var Parse = require('parse').Parse;
-Parse.initialize('NbTDXnwo9JwdCIz6LAIGEJxPqutcFnpZSolxi4Wo', 'yPgSpQoouc95OY70CYrUoUM2zBWkbOfpVz9uPFAx');
-var FBLogin = require('react-native-facebook-login');
-var LinearGradient = require('react-native-linear-gradient');
-var FacebookLoginManager = require('NativeModules').FacebookLoginManager;
-var Main = require('./App/Components/Main');
-
 var {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View,
+  AppRegistry
 } = React;
-
+var Login = require('./App/Components/Login');
 
 var hav2 = React.createClass({
-  getInitialState: function() {
-    return {
-      result: '...'
-    }
-  },
-  handleLogin: function(fbData) {
-    var credentials = fbData.credentials;
-    let authData = {
-      id: credentials.userId,
-      access_token: credentials.token,
-      expiration_date: credentials.tokenExpirationDate
-    };
-    Parse.FacebookUtils.logIn(authData, {
-      success: () => {
-        this.setState({loadingCurrentUser: false});
-      },
-      error: (user, error) => {
-        switch (error.code) {
-          case Parse.Error.INVALID_SESSION_TOKEN:
-            Parse.User.logOut().then(() => {
-              this.handleLogin(credentials);
-            });
-            break;
-          default:
-            this.setState({loadingCurrentUser: false});
-            alert(error.message);
-        }
-      }
-    });
-  },
-
-  render: function() {
-    let blue = '#7AC8DB';
-    let white = '#EFF0ED';
-    if (!this.state.loadingCurrentUser) {
-      return(
-        <Main
-          backgroundColor1={blue}
-          backgroundColor2={white}
-        />
-      )
-    }
-    var _this = this;
+  render: function()
+  {
     return (
-      <LinearGradient colors={[blue, white]} style={styles.linearGradient}>
-        <Text style={styles.titleOne}>Why show Gratitude?</Text>
-        <Text style={styles.titleOne}>Cause you</Text>
-        <Text style={styles.titleTwo}>Hav2</Text>
-
-        <FBLogin style={styles.loginButton}
-          permissions={["email","user_friends"]}
-          onLogin={this.handleLogin}
-          onLogout={function(){
-            console.log("Logged out.");
-            _this.setState({ user : null });
-          }}
-          onLoginFound={this.handleLogin}
-          onLoginNotFound={function(){
-            console.log("No user logged in.");
-            _this.setState({ user : null });
-          }}
-          onError={function(data){
-            console.log("ERROR");
-            console.log(data);
-          }}
-          onCancel={function(){
-            console.log("User cancelled.");
-          }}
-          onPermissionsMissing={function(data){
-            console.log("Check permissions!");
-            console.log(data);
-          }}
-        />
-      </LinearGradient>
-    );
-  }
-});
-
-var styles = StyleSheet.create({
-  linearGradient: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  titleOne: {
-    fontFamily: 'Allura-Regular',
-    fontSize: 35,
-  },
-  titleTwo: {
-    fontSize: 40,
-    fontWeight: 'bold'
-  },
-  loginButton: {
-    marginTop: 30
+      <Login />
+    )
   }
 });
 
